0c6cbbb2159408612981fbab41a8fdfcac9a0126
squall4-lol <sergio_bandera@hotmail.com>
2023-03-25 15:32:53 +0100

    feat: add getOne to employees, for check if employee exist

--- api/routes/empleados/controllers/index.js
+++ api/routes/empleados/controllers/index.js
@@ -1,12 +1,14 @@
 import Router from "express";
 import login from "./login";
 import create from "./create";
+import getOne from "./getOne";
 // import update from "./update";
 import list from "./list";
 
 const router = Router();
 router.route("/").post(create);
 router.route("/list").get(list);
+router.route("/oneEmployee").get(getOne)
 router.route("/login").post(login);
 // router.route("/").put(update);
 
