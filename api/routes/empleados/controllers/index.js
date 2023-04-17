da7a3f83058b6f6d9319489cc2f2c47f25e13cc2
squall4-lol <sergio_bandera@hotmail.com>
2023-04-17 21:02:39 +0200

    feat: funciona la api, faltará añadir mas endpoints en un futuro

--- api/routes/empleados/controllers/index.js
+++ api/routes/empleados/controllers/index.js
@@ -8,7 +8,7 @@ import list from "./list";
 const router = Router();
 router.route("/").post(create);
 router.route("/list").get(list);
-router.route("/oneEmployee").get(getOne)
+router.route("/oneEmployee").post(getOne)
 router.route("/login").post(login);
 // router.route("/").put(update);
 
