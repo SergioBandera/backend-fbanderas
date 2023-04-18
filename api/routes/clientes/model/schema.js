f989045f8a74ec9d4c7079cfd6fd635217086145
squall4-lol <sergio_bandera@hotmail.com>
2023-04-18 22:00:06 +0200

    feat: add client schema

--- api/routes/clientes/model/schema.js
+++ api/routes/clientes/model/schema.js
@@ -3,16 +3,28 @@ import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 const clientSchema = new Schema({
-    tipo:String,
-    empresa:{
-        empresa:String,
-        comercio:String,
-    },
     persona:{
         nombre:String,
-        apellidos:String,
+        primerApellido:String,
+        segundoApellido:String,
+        DNI:String,
+        Telefono:String,
+        segundoTelefono: String,
     },
-    
+    lugarTratamiento:{
+        piso:String,
+        comunidadVecinos:String,
+        nombreLocal:String,
+        Direccion:String,
+        Poblacion:String,
+        codigoPostal:Number,
+        metros:String,
+    },
+    empresa:{
+        nombreEmpresa:String,
+        direccionFacturacion:String,
+        NIF:String,
+    },
  });
 
  export default clientSchema;
